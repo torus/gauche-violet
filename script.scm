530b38e87d52e2e9d13f8bb53ec3db79dbf0bed8
Toru Hisai <toru@torus.jp>
2017-01-01 10:00:46 +0100

    clean up

--- script.scm
+++ script.scm
@@ -27,25 +27,12 @@
           (receive (auth path query frag) (uri-decompose-hierarchical abs-path)
             (let* ([path (uri-decode-string path :cgi-decode #t)]
                    [hdrs (rfc822-read-headers iport)]
-                   #;[host ($ rfc822-header-ref hdrs "host"
-                            $ sockaddr-name $ socket-getsockname csock)]
-                   #;[req (make-request line csock meth host path #f query hdrs)])
+                   )
               (print path)
               (print hdrs)
               (let1 content #`"HTTP/1.1 200 OK\nContent-Type: text/html\n\nhello ,client\n"
                     (enqueue! *response-queue* (cons client content))
                     (enqueue! *response-queue* (cons client 'eof)))
-              #;(unwind-protect
-               (match (find-handler path req app)
-                 [(handler req) (handler req app)]
-                 [_ (respond/ng req 404)])
-               ;; Clean temp files created by with-post-parameters
-               ;; NB: We can use a parameter, assuming one thread handles
-               ;; one request at a time.  If we introduce coroutines
-               ;; (a thread may switch handling requests), we need to avoid
-               ;; using cgi-temporary-files.
-               (for-each sys-unlink (cgi-temporary-files)))))]
-      #;[#/^[A-Z]+.*/ () (respond/ng (make-ng-request #`"[E] ,line" csock) 501)]
-      #;[else (respond/ng (make-ng-request #`"[E] ,line" csock) 400)]))
-
+              ))]
+      ))
   )
