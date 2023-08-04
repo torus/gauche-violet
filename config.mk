f9fa04db9e5993c68ecf17c8d0949dfe38126692
Toru Hisai <toru@torus.jp>
2023-08-04 17:15:43 +0900

    scaffolding

--- config.mk
+++ config.mk
@@ -2,19 +2,20 @@
 VERSION = 0.3.2-fix1
 
 # paths
-PREFIX = /usr/local
+PREFIX = $(PWD)/hack/local
 MANPREFIX = $(PREFIX)/share/man
 LIBPREFIX = $(PREFIX)/lib
 LIBDIR = $(LIBPREFIX)/violet
 
-GAUCHEINC = $(shell gauche-config -I)
-GAUCHELIB = $(shell gauche-config -l)
+GAUCHEINC = $(shell $(PREFIX)/bin/gauche-config -I)
+GAUCHELIB = $(shell $(PREFIX)/bin/gauche-config -l)
+GAUCHELIBDIR = $(shell $(PREFIX)/bin/gauche-config -L)
 UVINC = $(shell pkg-config --cflags libuv)
 UVLIB = $(shell pkg-config --libs libuv)
 
 # includes and libs
 INCS = $(GAUCHEINC) $(UVINC)
-LIBS = $(GAUCHELIB) $(UVLIB)
+LIBS = $(GAUCHELIBDIR) $(GAUCHELIB) $(UVLIB)
 
 # flags
 CFLAGS = -DVERSION=\"$(VERSION)\" -DLIBPREFIX=\"$(LIBPREFIX)\" -DLIBDIR=\"$(LIBDIR)\" -Wall -Werror
